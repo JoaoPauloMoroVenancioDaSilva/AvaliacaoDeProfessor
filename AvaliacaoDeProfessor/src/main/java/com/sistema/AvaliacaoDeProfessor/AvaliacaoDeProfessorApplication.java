d316d74a0a7e883d18a063968149a6475f14b578
JoaoPauloMoroVenancioDaSilva <joaopaulo.moro@edu.unifil.br>
2026-03-11 20:10:01 -0300

    Add files via upload

---
+++ AvaliacaoDeProfessor/src/main/java/com/sistema/AvaliacaoDeProfessor/AvaliacaoDeProfessorApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.AvaliacaoDeProfessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoDeProfessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoDeProfessorApplication.class, args);
+	}
+
+}
