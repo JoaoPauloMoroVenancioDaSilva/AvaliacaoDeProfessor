f0c1c9e6c26ea187a1da8f04ca283b85c3657600
Gabriel Dorta <mgabrieldorta@gmail.com>
2026-03-25 20:57:28 -0300

    Estruturas de Pasta e suas bases do backend
    Controller, Service, Security, Models

---
+++ backend/avaliacao/src/main/java/com/example/avaliacao/controller/dto/TokenDTO.java
@@ -0,0 +1,14 @@
+package com.example.avaliacao.controller.dto;
+
+public class TokenDTO {
+    private String token;
+    public TokenDTO(String token) {
+        this.token=token;
+    }
+    public String getToken(){
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ backend/avaliacao/src/main/java/com/example/avaliacao/model/repository/AlunoRepository.java
@@ -0,0 +1,4 @@
+package com.example.avaliacao.model.repository;
+
+public class AlunoRepository {
+}
