538919b41c7b0d75a42b0c051f389dcbf1485948
JoaoPauloMoroVenancioDaSilva <joaopaulo.moro@edu.unifil.br>
2026-04-05 22:59:35 -0300

    Implementação de FilterChain.java

--- backend/avaliacao/src/main/java/com/example/avaliacao/security/FilterChain.java
+++ backend/avaliacao/src/main/java/com/example/avaliacao/security/FilterChain.java
@@ -1,4 +1,91 @@
 package com.example.avaliacao.security;
 
+import com.example.avaliacao.model.repository.AlunoRepository;
+import com.example.avaliacao.model.repository.CoordenadorRepository;
+import com.example.avaliacao.model.repository.ProfessorRepository;
+import com.example.avaliacao.service.AlunoService;
+import com.example.avaliacao.service.CoordenadorService;
+import com.example.avaliacao.service.ProfessorService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.CachingUserDetailsService;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
 public class FilterChain {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, SecurityFilter filter) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorization -> {
+                    authorization.requestMatchers(HttpMethod.POST, "/api/login").permitAll();
+                    authorization.requestMatchers(HttpMethod.POST, "/api/coordenadores/**").hasRole("COORDENADOR");
+                    authorization.requestMatchers(HttpMethod.POST, "/api/professor/**").hasRole("PROFESSOR");
+                    authorization.requestMatchers(HttpMethod.POST, "/api/alunos/**").hasRole("ALUNO");
+                    authorization.requestMatchers(HttpMethod.POST, "/api/avaliacoes").hasAnyRole("COORDENADOR", "PROFESSOR");
+                    authorization.anyRequest().authenticated();
+                })
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(
+            ProfessorRepository professorRepo,
+            AlunoRepository alunoRepo,
+            CoordenadorRepository coordenadorRepo) {
+
+        return new CustomUserDetails(
+                professorRepo,
+                alunoRepo,
+                coordenadorRepo);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
 }
