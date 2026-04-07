275b9a4bfbea46a1beef04e86db24600e8837dc3
JoaoPauloMoroVenancioDaSilva <joaopaulo.moro@edu.unifil.br>
2026-04-07 19:35:42 -0300

    Create LoginDTO.java

---
+++ backend/avaliacao/src/main/java/com/example/avaliacao/controller/dto/LoginDTO.java
@@ -0,0 +1,16 @@
+package com.example.avaliacao.controller.dto;
+
+public class LoginDTO {
+
+    private String email;
+    private String senha;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+
+    }
+}
