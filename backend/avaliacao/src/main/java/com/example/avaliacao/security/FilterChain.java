a965cff33c4839c089d914dba8a42c0c32ded0ed
JoaoPauloMoroVenancioDaSilva <joaopaulo.moro@edu.unifil.br>
2026-04-07 19:31:17 -0300

    Correção FilterChain.java

--- backend/avaliacao/src/main/java/com/example/avaliacao/security/FilterChain.java
+++ backend/avaliacao/src/main/java/com/example/avaliacao/security/FilterChain.java
@@ -40,9 +40,9 @@ public class FilterChain {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorization -> {
                     authorization.requestMatchers(HttpMethod.POST, "/api/login").permitAll();
-                    authorization.requestMatchers(HttpMethod.POST, "/api/coordenadores/**").hasRole("COORDENADOR");
-                    authorization.requestMatchers(HttpMethod.POST, "/api/professor/**").hasRole("PROFESSOR");
-                    authorization.requestMatchers(HttpMethod.POST, "/api/alunos/**").hasRole("ALUNO");
+                    authorization.requestMatchers(HttpMethod.POST, "/api/coordenadores/**").permitAll();
+                    authorization.requestMatchers(HttpMethod.POST, "/api/professor/**").permitAll();
+                    authorization.requestMatchers(HttpMethod.POST, "/api/alunos/**").permitAll();
                     authorization.requestMatchers(HttpMethod.POST, "/api/avaliacoes").hasAnyRole("COORDENADOR", "PROFESSOR");
                     authorization.anyRequest().authenticated();
                 })
