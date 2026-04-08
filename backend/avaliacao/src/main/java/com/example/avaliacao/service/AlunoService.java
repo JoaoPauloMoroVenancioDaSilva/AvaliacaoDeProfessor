cfe55b399f8bd7993678ec8af377127e8e90f783
JoaoPauloMoroVenancioDaSilva <joaopaulo.moro@edu.unifil.br>
2026-04-08 18:53:39 -0300

    Update AlunoService.java

--- backend/avaliacao/src/main/java/com/example/avaliacao/service/AlunoService.java
+++ backend/avaliacao/src/main/java/com/example/avaliacao/service/AlunoService.java
@@ -10,7 +10,9 @@ import com.example.avaliacao.model.repository.ProfessorRepository;
 import com.example.avaliacao.model.repository.SolicitacaoProfRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 
 import java.util.List;
