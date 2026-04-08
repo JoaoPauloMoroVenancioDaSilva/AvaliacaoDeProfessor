341f27456f07b2507085c55af9ba3384b9b3a6c9
YuriLotzB <yuribrun@edu.unifil.br>
2026-04-08 18:56:30 -0300

    Update AvaliacaoService.java

--- backend/avaliacao/src/main/java/com/example/avaliacao/service/AvaliacaoService.java
+++ backend/avaliacao/src/main/java/com/example/avaliacao/service/AvaliacaoService.java
@@ -10,6 +10,8 @@ import com.example.avaliacao.model.repository.ProfessorRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
