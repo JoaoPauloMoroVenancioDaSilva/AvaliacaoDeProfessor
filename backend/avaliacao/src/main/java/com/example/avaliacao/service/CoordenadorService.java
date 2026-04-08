e78128918cb2dda392505c5fcdd9833b11a53dd4
YuriLotzB <yuribrun@edu.unifil.br>
2026-04-08 18:56:50 -0300

    Update CoordenadorService.java

--- backend/avaliacao/src/main/java/com/example/avaliacao/service/CoordenadorService.java
+++ backend/avaliacao/src/main/java/com/example/avaliacao/service/CoordenadorService.java
@@ -7,6 +7,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
